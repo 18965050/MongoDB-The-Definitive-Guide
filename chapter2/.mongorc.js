745cf2ddcf95410ff76b450dd24487c970f16415
18965050 <18965050@qq.com>
2016-03-07 17:37:09 +0800

    代码整理

---
+++ chapter2/.mongorc.js
@@ -0,0 +1,17 @@
+print("Welcome To MongDB World");
+
+EDITOR="/bin/vi"
+
+prompt = function() {
+    if (typeof db == 'undefined') {
+        return '(nodb)> ';
+    }
+// Check the last db operation
+    try {
+        db.runCommand({getLastError:1});
+    }
+    catch (e) {
+        print(e);
+    }
+    return db+"> ";
+};
